57be14a5cbc51d6c04ec8b68f2ef6c4140421af6
David Wu <KyosukeNo1@gmail.com>
2024-02-09 01:30:24 -0800

    Update dist folder

--- dist/tui-image-editor.js
+++ dist/tui-image-editor.js
@@ -51685,11 +51685,11 @@ var ImageTracer = /*#__PURE__*/function () {
         });
       },
       download: function download() {
-        var _this$ui$onClickFinis, _this$ui;
+        var _this$ui$options$onCl, _this$ui$options;
         var dataURL = _this.toDataURL();
         var imageName = _this.getImageName();
         var blob, type, w;
-        (_this$ui$onClickFinis = (_this$ui = _this.ui).onClickFinishButton) === null || _this$ui$onClickFinis === void 0 || _this$ui$onClickFinis.call(_this$ui, base64ToBlob(dataURL));
+        (_this$ui$options$onCl = (_this$ui$options = _this.ui.options).onClickFinishButton) === null || _this$ui$options$onCl === void 0 || _this$ui$options$onCl.call(_this$ui$options, base64ToBlob(dataURL));
         if (isSupportFileApi() && window.saveAs) {
           blob = base64ToBlob(dataURL);
           type = blob.type.split('/')[1];
